4222887cf223a3cd45656ab1daba960656e24fa2
Kaustubh Ajgaonkar <kaustubhajgaonkar43@gmail.com>
2023-03-22 20:27:26 +0530

    added git ignore

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 from twilio.rest import Client
 
 # Fill out the below info from your account
-account_sid = '[ACCOUNT_SID]'
-auth_token = '[ACCOUNT TOKEN]'
+account_sid = 'AC2b40a43e6fe45c10ae9dde2821b927ec'
+auth_token = 'e8b3b49b14b04ea9dc4169d3ed49c279'
 client = Client(account_sid, auth_token)
-SER_ID = '[SERVICE ID]'
+SER_ID = 'MG1130caa8dc16ab6aee1431d658b99261'
 
 
 # Fill in the recepient phone number with country code and message body
-BODY = '[BODY]'
-R_NUM = '[Recepient number]'
+BODY = 'Hello Amigos.'
+R_NUM = '+919096730500'
 
 message = client.messages.create(
-    messaging_service_sid=SER_ID,
-    body=BODY,
+    messaging_service_sid='MG1130caa8dc16ab6aee1431d658b99261',
+    body='Hello Amigos how are you doing?',
     to=R_NUM
 )
 
